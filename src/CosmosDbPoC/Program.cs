3cca006e675c3d6f6ef1ce5868f1174011f5128e
Jonny Olliff-Lee <jonny.ollifflee@gmail.com>
2022-07-30 16:34:22 +0100

    Feat(settings) | Adds setting into environment variables

--- src/CosmosDbPoC/Program.cs
+++ src/CosmosDbPoC/Program.cs
@@ -31,6 +31,12 @@ rootCommand.AddOption(primaryKey);
 
 rootCommand.SetHandler((endpointValue, primaryKeyValue) =>
 {
+    if (endpointValue is not null)
+        Environment.SetEnvironmentVariable("CosmosDb__Outbox__Endpoint", endpointValue, EnvironmentVariableTarget.User);
+    
+    if (primaryKeyValue is not null)
+        Environment.SetEnvironmentVariable("CosmosDb__Outbox__PrimaryKey", primaryKeyValue, EnvironmentVariableTarget.User);
+    
     var name = AnsiConsole.Ask<string>("What's your [green]name[/]?");
     var email = AnsiConsole.Ask<string>("What's your [green]email[/]?");
 
